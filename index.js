14bb4d6fecc4e12124818f567de42a9843f31c50
John Brett <john.brett@d4h.org>
2014-07-10 23:31:09 +0100

    Added mapped index functionality

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var Level = require('level')
 var Sublevel = require('level-sublevel')
+var MappedIndex = require('level-mapped-index')
 
 exports.register = function(plugin, options, next) {
 
 	var path = options.path || './data'
     var config = options.config || {}
 
-	var db = Sublevel(Level(path, config))
+	var db = MappedIndex(Sublevel(Level(path, config)))
 
 	plugin.expose('db', db)
 	next()
