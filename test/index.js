9e75916e8608209a86742c05f5190cacf523f46d
John Brett <johnbrett7@gmail.com>
2014-08-14 11:38:16 +0100

    fix tests

--- test/index.js
+++ test/index.js
@@ -1,55 +1,56 @@
 var Lab = require('lab')
 var Hapi = require('hapi')
 
-Lab.experiment('plugin', function () {
-    var server = null;
+Lab.experiment('hapi-level', function () {
+    var server = null
 
     Lab.beforeEach(function (done) {
-        server = new Hapi.Server('localhost', 8080);
-        done();
+        server = new Hapi.Server('localhost', 8080)
+        done()
     });
 
     Lab.afterEach(function (done) {
+
         var stopServer = function(){
              server.stop(function(){
-                 server = null;
-                 done();
+                 server = null
+                 done()
              });
-        }
+        };
 
         if(server.plugins['hapi-level']){
             server.plugins['hapi-level'].db.close(stopServer)
         } else {
-            stopServer
+            stopServer()
         }
     });
 
-    Lab.test('Plugin can register with default settings', function (done) {
+    Lab.test('can register with default settings', function (done) {
         server.pack.register({
             plugin: require('../index')
         }, function(err) {
-            Lab.expect(err).to.equal(undefined);
-            done();
+            Lab.expect(err).to.equal(undefined)
+            done()
         })
     });
 
-    Lab.test('Plugin can register with data directory set', function (done) {
+    Lab.test('can register with data directory set', function (done) {
         server.pack.register({
             plugin: require('../index'),
             options: {
                 path: './data'
             }
         }, function(err) {
-            Lab.expect(err).to.equal(undefined);
-            done();
+            Lab.expect(err).to.equal(undefined)
+            done()
         })
     });
 
-    Lab.test('Testing Level operations', function (done) {
+    Lab.test('can use regular level operations', function (done) {
         server.pack.register({
             plugin: require('../index'),
             options: {
-                path: './data_test2'
+                path: './data'
             }
         }, function(err) {
             Lab.expect(err).to.equal(undefined);
@@ -59,19 +60,19 @@ Lab.experiment('plugin', function () {
 
                 db.put('name', 'Level', function (err) {
                     db.get('name', function (err, value) {
-                        Lab.expect(value).to.equal('Level');
-                        done();
+                        Lab.expect(value).to.equal('Level')
+                        done()
                     })
                 })
             })
         })
     });
 
-    Lab.test('Testing initialisaation with extra config', function (done) {
+    Lab.test('can specify extra level config such as valueEncoding', function (done) {
         server.pack.register({
             plugin: require('../index'),
             options: {
-                path: './data_test2',
+                path: './data/test2',
                 config: {
                     valueEncoding: 'json'
                 }
@@ -84,19 +85,19 @@ Lab.experiment('plugin', function () {
 
                 db.put('name', 'Level', function (err) {
                     db.get('name', function (err, value) {
-                        Lab.expect(value).to.equal('Level');
-                        done();
+                        Lab.expect(value).to.equal('Level')
+                        done()
                     })
                 })
             })
         })
     });
 
-    Lab.test('Testing getSublevel method functions', function (done) {
+    Lab.test('can use sublevel functions as expected', function (done) {
         server.pack.register({
             plugin: require('../index'),
             options: {
-                path: './data_test2',
+                path: './data/test2',
                 config: {
                     valueEncoding: 'json'
                 }
@@ -112,9 +113,9 @@ Lab.experiment('plugin', function () {
                 users.put('name', {'username':'User1', 'id': 1}, function (err) {
                     users.get('name', function (err, value) {
                         Lab.expect(err).to.equal(null);
-                        Lab.expect(JSON.stringify(value)).to.equal(JSON.stringify({'username':'User1', "id": 1}));
-                        done();
-                    })
+                        Lab.expect(JSON.stringify(value)).to.equal(JSON.stringify({'username':'User1', "id": 1}))
+                        done()
+                    });
                 })
             })
         })
